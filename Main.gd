0d90d1e5d59d8790320fd05a34cdc3b33c9ba8cb
Pavel Grebnev <ruwhynot@gmail.com>
2023-10-01 11:45:54 +0200

    Added ability to record and replay action patterns

--- Main.gd
+++ Main.gd
@@ -3,12 +3,24 @@ extends Node
 export(PackedScene) var mob_scene
 var score
 
+var time = 0.0
+
+# debug_only
+var debug_recorded_action_times = []
+var debug_recorded_action_positions = []
+var is_replay = false
+
+const is_invinsible = false
 
 func _ready():
 	randomize()
 
 
 func game_over():
+	if is_invinsible:
+		return
+		
+	$Player.hide() # Player disappears after being hit.
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
@@ -27,6 +39,7 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	$ColorRect.hide()
+	time = 0.0
 	#$Music.play()
 
 
@@ -64,3 +77,37 @@ func _on_ScoreTimer_timeout():
 func _on_StartTimer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+
+
+func _process(delta):
+	if OS.is_debug_build():
+		if is_replay:
+			for i in range(0, len(debug_recorded_action_times)):
+				var i_time = debug_recorded_action_times[i]
+				if time < i_time and time + delta >= i_time:
+					var pos = debug_recorded_action_positions[i]
+					print("Replaying pos {0}, {1}".format([pos.x, pos.y]))
+					break
+	
+	time += delta
+	
+
+
+func _input(event):
+	if OS.is_debug_build():
+		if event is InputEventMouseButton:
+			if event.button_index == BUTTON_RIGHT and event.pressed:
+				print("Reset recording")
+				time = 0.0
+				debug_recorded_action_times = []
+				debug_recorded_action_positions = []
+				is_replay = false
+			if event.button_index == BUTTON_MIDDLE and event.pressed:
+				print("Replaying")
+				time = 0.0
+				is_replay = true
+			elif event.button_index == BUTTON_LEFT and event.pressed and not is_replay:
+				print("Record click: [{0}, Vector2({1}, {2})]".format([time, event.position.x, event.position.y]))
+				debug_recorded_action_times.append(time)
+				debug_recorded_action_positions.append(event.position)
+				
