12730acc2ffcfaeb2739d05c680994bae33c3e1e
Pavel Grebnev <ruwhynot@gmail.com>
2023-10-01 21:16:02 +0200

    Added menu art

--- Player.gd
+++ Player.gd
@@ -40,14 +40,15 @@ func _process(delta):
 	
 	var velocity = Vector2.ZERO # The player's movement vector.
 
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+	if !is_dead:
+		if Input.is_action_pressed("move_right"):
+			velocity.x += 1
+		if Input.is_action_pressed("move_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("move_down"):
+			velocity.y += 1
+		if Input.is_action_pressed("move_up"):
+			velocity.y -= 1
 
 	dash_cooldown_left -= delta
 	if dash_cooldown_left < 0.0:
